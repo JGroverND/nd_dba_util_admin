4d708e8c520f4252203b17f21ae3fe294820e09f
John Grover <jgrover@nd.edu>
2022-11-14 15:23:00 -0500

    moved rename role to package

--- nd_dba_oracle_account_util_body.sql
+++ nd_dba_oracle_account_util_body.sql
@@ -889,6 +889,66 @@ end ora_acct_revoke_object_priv;
 
 -- ora_acct_clone_acct(acct, clone)
 -- -----------------------------------------------------------------------------
+
+procedure ora_role_rename (p_old_role in sys.dba_roles.role%type,
+                           p_new_role in sys.dba_roles.role%type) as
+
+  cursor priv_cursor is 
+  select 'create role ' || upper(p_new_role) sqlcmd
+    from DUAL
+  union
+  select 'grant ' || granted_role || ' to ' || upper(p_new_role) || decode(admin_option, 'YES', ' with admin option' ,'')
+    from dba_role_privs
+   where grantee = upper(p_old_role)
+  union
+  select 'grant ' || PRIVILEGE || ' to ' || upper(p_new_role) || decode(admin_option, 'YES', ' with admin option' ,'')
+    from dba_sys_privs
+   where grantee = upper(p_old_role)
+  union
+  select 'grant ' || PRIVILEGE || ' on ' || owner || '.' || table_name || ' to '  || upper(p_new_role) || decode(grantable, 'YES', ' with grant option', '')
+    from dba_tab_privs
+   where grantee = upper(p_old_role)
+  union
+  where grantee = upper(p_old_role) ;
+   
+  cursor grant_cursor is
+  select 'grant ' || upper(p_new_role) || ' to ' || grantee || decode(admin_option, 'YES', ' with admin option', '') sqlcmd
+    from dba_role_privs,
+         dba_users
+   where granted_role = upper(p_old_role) 
+     and username = grantee
+     and profile not like 'ND_SYS%';
+   
+  cursor revoke_cursor is
+  select 'revoke ' || upper(p_old_role) || ' from ' || grantee sqlcmd
+    from dba_role_privs,
+         dba_users
+   where granted_role = upper(p_old_role) 
+     and username = grantee
+     and profile not like 'ND_SYS%';
+
+begin
+    
+    for my_cursor_rec in priv_cursor
+    loop
+      -- dbms_output.put_line (my_cursor_rec.sqlcmd);
+      execute immediate my_cursor_rec.sqlcmd ;
+    end loop;
+
+    for my_cursor_rec in grant_cursor
+    loop
+      --dbms_output.put_line (my_cursor_rec.sqlcmd);
+      execute immediate my_cursor_rec.sqlcmd ;
+    end loop;
+
+    for my_cursor_rec in revoke_cursor
+    loop
+      --dbms_output.put_line (my_cursor_rec.sqlcmd);
+      execute immediate my_cursor_rec.sqlcmd ;
+    end loop;  
+end ora_role_rename;
+
+-- -----------------------------------------------------------------------------
 --                                                   E N D   O F   P A C K A G E
 -- -----------------------------------------------------------------------------
 end nd_dba_oracle_account_util;
